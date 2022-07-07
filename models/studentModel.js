c7f6d5e3c1e4ba6a75a8c5cc29d32e075f1895ac
JAIMIN KATVA <98204346+jaiminkatva@users.noreply.github.com>
2022-07-07 17:55:13 +0530

    Delete models directory

--- models/studentModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-
-    const Student = sequelize.define("student", {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        active: DataTypes.BOOLEAN
-    })
-    return Student
-}
